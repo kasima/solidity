0ac2a816f2676796ce3ed1462e08293321e9476c
chriseth <chris@ethereum.org>
2018-08-07 21:08:40 +0200

    Update tests.

--- test/libsolidity/syntaxTests/dataLocations/function_return_parameters_with_data_location_fine.sol
+++ test/libsolidity/syntaxTests/dataLocations/function_return_parameters_with_data_location_fine.sol
@@ -3,5 +3,5 @@ contract C {
     function f() private pure returns(uint[] memory, uint[] storage b) { b = b; }
     function g() internal pure returns(uint[] memory, uint[] storage b) { b = b; }
     function h() public pure returns(uint[] memory) {}
-    function i() external pure returns(uint[] calldata) {}
+    function i() external pure returns(uint[] memory) {}
 }
